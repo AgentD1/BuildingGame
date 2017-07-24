d1d7719a5e1a8e204e6c5b9528fd6654dacd32cb
DESKTOP-NRTU110\jampa <jamparker7@yahoo.com>
2017-07-24 13:36:33 -0400

    First copy of game

---
+++ Assets/Scripts/BlockManager.cs
@@ -0,0 +1,200 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockManager : MonoBehaviour {
+
+    public int width = 10;
+    public int height = 10;
+    public GameObject blockPrefab;
+    public GameObject chunkPrefab;
+    Dictionary<Vector3, Block> blocks;
+    public const int CHUNKSIZE = 15;
+    public GameObject player;
+    Dictionary<Vector2,Chunk> chunks;
+    List<Chunk> previouslyRenderedChunks;
+    int chunksVisibleInRenderDistance;
+    public int blockCount = 0;
+    public float renderDistance = 64;
+
+    // Use this for initialization
+    void Start () {
+        chunksVisibleInRenderDistance = Mathf.RoundToInt(renderDistance / CHUNKSIZE);
+        chunks = new Dictionary<Vector2, Chunk>();
+        previouslyRenderedChunks = new List<Chunk>();
+        blocks = new Dictionary<Vector3, Block>();
+        for (int i = -3; i < 4; i++) {
+            for (int j = -3; j < 4; j++) {
+                GenerateChunk(new Vector2(i, j));
+            }
+        }
+	}
+	
+    public void BlockClick(bool breaking,RaycastHit hit) {
+        if (breaking) {
+//            DestroyBlock(hit.transform.position);
+        } else {
+            int clickChunkCoordX = Mathf.CeilToInt(player.transform.position.x / CHUNKSIZE);
+            int clickChunkCoordY = Mathf.CeilToInt(player.transform.position.z / CHUNKSIZE);
+            CreateBlock(hit.transform.position + hit.normal);
+        }
+        
+    }
+    
+    Chunk GenerateChunk(Vector2 position) {
+        CreateChunk(position);
+        for (int x = -CHUNKSIZE / 2; x < CHUNKSIZE / 2 + 1; x++) { 
+            for (int y = -CHUNKSIZE / 2; y < CHUNKSIZE / 2 + 1; y++) {
+                CreateBlock(new Vector3(x + (position.x * CHUNKSIZE), 0, y + (position.y * CHUNKSIZE)));
+            }
+        }
+        return chunks[position];
+    }
+
+    Chunk CreateChunk(Vector2 position) {
+        if (chunks.ContainsKey(position)) {
+            Debug.Log("Chunk already exists at " + position);
+            return chunks[position];
+        } else {
+            GameObject go = Instantiate(chunkPrefab, new Vector3(position.x * CHUNKSIZE, 0, position.y * CHUNKSIZE), Quaternion.identity, transform);
+            Chunk chunk = new Chunk(position, go);
+            chunks.Add(position, chunk);
+            return chunk;
+        }
+    }
+
+    Block CreateBlock(Vector3 position) {
+        if (chunks.ContainsKey(new Vector2(Mathf.Round(position.x / CHUNKSIZE), Mathf.Round(position.z / CHUNKSIZE)))) {
+            GameObject go = Instantiate(blockPrefab, position, Quaternion.identity, chunks[new Vector2(Mathf.Round(position.x / CHUNKSIZE), Mathf.Round(position.z / CHUNKSIZE))].gameObject.transform);
+            Block block = new Block(position, go);
+            blocks.Add(position, block);
+            return block;
+        } else if (blocks.ContainsKey(position)) {
+            Debug.LogError("Block already exists at " + position);
+            return blocks[position];
+        } else {
+            Debug.LogError("No chunk at position " + new Vector2(Mathf.Round(position.x / CHUNKSIZE), Mathf.Round(position.z / CHUNKSIZE)));
+            return null;
+        }
+    }
+
+    //bool DestroyBlock(Vector3 position) {
+
+    //}
+
+
+    /*
+    public void CreateChunk(Vector3 position) {
+
+        GameObject go = Instantiate(chunkPrefab, new Vector3(position.x*CHUNKSIZE,0,position.z*CHUNKSIZE), Quaternion.identity, transform);
+        
+        chunks.Add(new Vector2(position.x, position.z), new Chunk(position, go));
+        Debug.Log(chunks[new Vector2(position.x, position.z)]);
+    }
+
+    public void CreateBlock(Vector3 position, GameObject chunk) {
+        GameObject go = Instantiate(blockPrefab, position, Quaternion.identity, chunk.transform);
+        blocks.Add(position, new Block(position,go));
+        blockCount++;
+        
+    }
+
+    public void DestroyBlock(Vector3 position) {
+        Destroy(blocks[position].gameObject, 0.001f);
+        blockCount--;
+        blocks.Remove(position);
+    }
+
+    void UpdateVisibleChunks() {
+
+        for (int i = 0; i < previouslyRenderedChunks.Count; i++) {
+            previouslyRenderedChunks[i].SetVisible(false);
+        }
+        previouslyRenderedChunks.Clear();
+
+        int currentChunkCoordX = Mathf.CeilToInt(player.transform.position.x / CHUNKSIZE);
+        int currentChunkCoordY = Mathf.CeilToInt(player.transform.position.z / CHUNKSIZE);
+
+        for (int yOffset = -chunksVisibleInRenderDistance; yOffset <= chunksVisibleInRenderDistance; yOffset++) {
+            for (int xOffset = -chunksVisibleInRenderDistance; xOffset <= chunksVisibleInRenderDistance; xOffset++) {
+                Vector2 viewedChunkCoord = new Vector2(currentChunkCoordX + xOffset, currentChunkCoordY + yOffset);
+                Debug.Log(chunks.Count);
+                if (chunks.ContainsKey(viewedChunkCoord)) {
+                    chunks[viewedChunkCoord].UpdateChunkVisibility(player.transform.position,renderDistance);
+                    if (chunks[viewedChunkCoord].gameObject.activeSelf) {
+                        previouslyRenderedChunks.Add(chunks[viewedChunkCoord]);
+                    }
+                } else {
+                    GenerateChunk(new Vector3(viewedChunkCoord.x, 0, viewedChunkCoord.y));
+                }
+
+            }
+        }
+    }
+    void GenerateChunk(Vector3 position) {
+        if (chunks.ContainsKey(new Vector2(position.x, position.z))) { return; }
+        CreateChunk(new Vector2(position.x, position.z));
+
+        for (int i = (int) position.x*CHUNKSIZE; i < position.x*CHUNKSIZE + CHUNKSIZE; i++) {
+            for (int j = (int)position.y * CHUNKSIZE; j < position.y * CHUNKSIZE + CHUNKSIZE; j++) {
+                CreateBlock(new Vector3(i, 0, j), chunks[new Vector2(position.x, position.z)].gameObject);
+            }
+        }
+
+    }
+
+    */
+
+    // Update is called once per frame
+    void Update () {
+        //UpdateVisibleChunks();
+	}
+
+ 
+}
+
+
+public class Block {
+    public GameObject gameObject;
+
+    public Vector3 pos;
+    
+    public Block(Vector3 position, GameObject go) {
+        pos = position;
+        gameObject = go;
+    }
+    public Block(int xPos, int yPos, int zPos, GameObject go) {
+        pos = new Vector3(xPos, yPos, zPos);
+        gameObject = go;
+    }
+    public Block(Vector3 position) {
+        pos = position;
+    }
+    public Block(int xPos, int yPos, int zPos) {
+        pos = new Vector3(xPos, yPos, zPos);
+    }
+}
+
+public class Chunk {
+    public GameObject gameObject;
+    public Vector2 pos;
+    Bounds bounds;
+
+    public Chunk(Vector2 position, GameObject go) {
+        pos = position;
+        gameObject = go;
+        bounds = new Bounds(new Vector3(pos.x * BlockManager.CHUNKSIZE, 0, pos.y * BlockManager.CHUNKSIZE), new Vector3(BlockManager.CHUNKSIZE, 500, BlockManager.CHUNKSIZE));
+        SetVisible(false);
+    }
+
+    public void UpdateChunkVisibility(Vector3 playerPos, float renderDistance) {
+        float viewerClosestDist = Mathf.Sqrt(bounds.SqrDistance(playerPos));
+        bool visible = viewerClosestDist >= renderDistance;
+        SetVisible(visible);
+    }
+
+    public void SetVisible(bool isVisible) {
+        gameObject.SetActive(isVisible);
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float movementSpeed = 5f;
+    public float sensitivityX = 10f;
+    public float sensitivityY = 10f;
+    public float jumpHeight = 2f;
+    float distToGround;
+    Rigidbody rb;
+    public float minimumX = -360F;
+    public float maximumX = 360F;
+    public GameObject cameraGO;
+    public float minimumY = -90f;
+    public float maximumY = 90f;
+    float rotationY = 0F;
+    public float reach = 4f;
+    public BlockManager blockManager;
+    public LayerMask mask;
+    bool mouse1down = false;
+    bool mouse2down = false;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        distToGround = GetComponent<Collider>().bounds.extents.y;
+    }
+
+
+    void Update() {
+
+
+        if (Input.GetMouseButtonDown(0)) {
+            mouse1down = true;
+        }
+        if (Input.GetMouseButtonDown(1)) {
+            mouse2down = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Tab)) {
+            if (Cursor.lockState == CursorLockMode.None) {
+                Cursor.lockState = CursorLockMode.Locked;
+            } else {
+                Cursor.lockState = CursorLockMode.None;
+            }
+            Debug.Log(Cursor.lockState);
+        }
+    }
+    
+    // Update is called once per frame
+    void FixedUpdate () {
+        float y = Input.GetAxis("Vertical") * movementSpeed;
+        float x = Input.GetAxis("Horizontal") * movementSpeed;
+
+        rb.velocity = transform.TransformDirection(new Vector3(x, rb.velocity.y, y));
+
+        float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+
+        rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+        rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+        transform.localEulerAngles = new Vector3(0, rotationX, 0);
+        cameraGO.transform.localEulerAngles = new Vector3(-rotationY, 0, 0);
+        Debug.DrawRay(cameraGO.transform.position, cameraGO.transform.rotation.eulerAngles);
+        if (mouse1down) {
+            RaycastHit hit;
+            Ray ray = cameraGO.GetComponent<Camera>().ScreenPointToRay(new Vector3(cameraGO.GetComponent<Camera>().pixelWidth/2, cameraGO.GetComponent<Camera>().pixelHeight / 2, 0f));  //new Ray(cameraGO.transform.position, cameraGO.transform.rotation.eulerAngles);
+            if(Physics.Raycast(ray, out hit, reach, mask)) {
+                blockManager.BlockClick(false, hit);
+            } else {
+                Debug.Log("Not detected");
+            }
+            mouse1down = false;
+        }
+        if (mouse2down) {
+            RaycastHit hit;
+            Ray ray = cameraGO.GetComponent<Camera>().ScreenPointToRay(new Vector3(cameraGO.GetComponent<Camera>().pixelWidth / 2, cameraGO.GetComponent<Camera>().pixelHeight / 2, 0f));  //new Ray(cameraGO.transform.position, cameraGO.transform.rotation.eulerAngles);
+            if (Physics.Raycast(ray, out hit, reach, mask)) {
+                blockManager.BlockClick(true, hit);
+            }
+            mouse2down = false;
+        }
+
+
+        if (Input.GetButton("Jump")) {
+            if (isGrounded()) {
+                rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y + CalculateJumpVerticalSpeed(), rb.velocity.z);
+            }
+        }
+    }
+
+    bool isGrounded() {
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
+    }
+
+    private float CalculateJumpVerticalSpeed() {
+        return Mathf.Sqrt(2 * jumpHeight * Mathf.Abs(Physics.gravity.y));
+    }
+
+
+}
